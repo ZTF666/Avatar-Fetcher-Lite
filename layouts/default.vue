c9f51122682c6766bac1801ce4695a76434429e4
ZTF666 <ZTF666@protonmail.ch>
2020-10-01 21:20:37 +0100

    working project :heart:

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,6 +21,45 @@
         <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
+    <!-- appbar phone -->
+    <v-app-bar :clipped-left="clipped" fixed app class="hidden-sm-and-up">
+      <v-img
+        alt="Logo"
+        class="shrink mr-2"
+        contain
+        :src="logo"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title v-text="title" />
+      <v-spacer></v-spacer>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <!-- <v-list-item> -->
+          <v-list-item-content>
+            <!-- <v-spacer></v-spacer> -->
+            <v-btn text nuxt to="/">Home</v-btn>
+            <v-btn text nuxt to="inspire">About Me</v-btn>
+            <v-btn
+              href="https://github.com/ZTF666/Avatar-Fetcher-Lite"
+              target="_blank"
+              text
+            >
+              <v-icon>mdi-github</v-icon>
+            </v-btn>
+          </v-list-item-content>
+
+          <!-- </v-list-item> -->
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <!-- end -->
     <v-main class="bg">
       <v-container>
         <nuxt />
