71ed8a46e15c2ffd2094710006ccef5aef0a314f
ZTF666 <ZTF666@protonmail.ch>
2020-10-27 03:19:34 +0100

    :heart:

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
+      <v-alert class="dark error--text">
+      <center>Mozilla Users : set "Enhanced Tracking Protection" to standard, or you'll get false negatives on searches.
+</center>
+    </v-alert>
       <Profile />
     </v-col>
   </v-row>
